ac68dc004ad699b36f942d4764d96bb3612fc28e
aa-tier-list-maker <eyacorkett3@gmail.com>
2025-10-14 17:51:06 +0800

    Update common.js

--- editableJs/common.js
+++ editableJs/common.js
@@ -8,22 +8,31 @@ var imgGamesFolder = [
     ["aa", 0, ""],
     ["aa1", 0, "Phoenix Wright"],
     ["aa2", 0, "Justice for All"],
+    
     ["aa3", 0, "Trials and Tribulations"],
     ["aa4", 31, "Apollo Justice"],
-    ["aa4ex", , "Apollo Justice (Extras)"],
+    
+    ["aa4ex", 0, "Apollo Justice (Extras)"],
     ["aa5", 0, "Dual Destinies"],
+    
     ["aa6", 0, "Spirit of Justice"],
     ["aai1", 0, "AAI: Miles Edgeworth"],
+    
     ["aai2", 0, "AAI2: Prosecutor's Gambit"],
     ["tgaa1", 32, "TGAA: Adventures"],
+    
     ["tgaa1ex", 1, "TGAA: Adventures (Extras)"],
     ["tgaa2", 22, "TGAA2: Resolve"],
+    
     ["tgaa2ex", 9, "TGAA2: Resolve (Extras)"],
     ["aota", 39, "Attorney of the Arcane"],
+    
     ["taj", 15, "Trials after Justice"],
     ["coc", 21, "Contempt of Court"],
+    
     ["tsub", 0, "Turnabout Substitution"],
     ["coi", 0, "Conflict of Interest"],
+    
     ["ryan", 0, "Ryan Mendoza"]
 ];
 
